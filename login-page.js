55a246e1c30930ee9a3097a3638fcb15f943a4b5
perthDDD <140643274+perthDDD@users.noreply.github.com>
2023-08-10 05:24:10 +0800

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -7,9 +7,9 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "perthTGW500" && password === "uniteAs1Acheive") {
+    if (username === "uniteAsOne" && password === "achieveOurGoal") {
         alert("You have successfully logged in.");
-        window.location.href = "https://funky-wave-252.notion.site/TGW-EDU-a5b9146a42804d269e86dcbe55c3f746?pvs=4";
+        window.location.href = "https://perth-heavenly-library.super.site/";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
